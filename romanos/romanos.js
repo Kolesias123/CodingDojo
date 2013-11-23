545e857bce0cf5b52e4863cae831a2ac0ee3f658
Kolesias123 <ivan-bravo@infosmart.mx>
2013-11-23 14:26:45 -0600

    Optimización del código.

--- romanos/romanos.js
+++ romanos/romanos.js
@@ -1,47 +1,50 @@
+// Enteros y sus letras en Romano.
+var Arreglo = {
+	'M': 1000,
+	'CM': 900,
+	'D': 500,
+	'CD': 400,
+	'C': 100,
+	'XC': 90,
+	'L': 50,
+	'XL': 40,
+	'X': 10,
+	'IX': 9,
+	'V': 5,
+	'IV': 4,
+	'I': 1,
+};
+
+/**
+ * Transforma el numero entero a numero romano.
+ */
 function ARomano()
 {
-	var pNum = document.getElementById('valor').value;
+	// Obtenemos el entero que queremos transformar.
+	var pNum = parseInt(document.getElementById('valor').value);
 
+	// ¡Es necesario un numero!
 	if ( pNum <= 0 )
-		return alert('Escribe un numero valido.');
-
-	// Enteros y sus letras en Romano.
-	var Arreglo = {
-		1: 'I',
-		4: 'IV',
-		5: 'V',
-		9: 'IX',
-		10: 'X',
-		50: 'L',
-		40: 'XL',
-		90: 'XC',
-		100: 'C',
-		400: 'CD',
-		500: 'D',
-		900: 'CM',
-		1000: 'M'
-	};
+		return alert('Escribe un numero valido mayor a 0.');
 
 	// Resultado final (Aqui iran las letras romanas)
 	var pResult = '';
 
-	for ( i = 2000; i > 0; --i )
+	for ( var i in Arreglo )
 	{
-		// Este numero no es valido para convertir.
-		if ( Arreglo[i] == undefined )
-			continue;
+		var pCantidad = Arreglo[i];
 
 		// Division entre la cantidad a convertir y el numero que podemos convertir a romano.
-		var pTmp = pNum / i;
+		var pTmp = pNum / pCantidad;
 
-		// La division ha dado mayor a 1, perfecto.
+		// La division ha dado mayor o igual a 1, perfecto.
 		if ( pTmp >= 1 )
 		{
 			// Ponemos la cantidad de letras adecuada a la division.
 			for ( f = 1; f <= pTmp; ++f )
 			{
-				pNum 	-= i;
-				pResult += Arreglo[i];
+				pNum 	-= pCantidad;
+				pResult += i;
 			}
 		}
 	}
