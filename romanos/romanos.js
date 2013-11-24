00d25f94de1fbaab31e64388627cfd8014d79091
Kolesias123 <ivan-bravo@infosmart.mx>
2013-11-24 05:01:10 -0600

    Optimización del código por: @AlgusDark Soporte para numeros mayores a 4,000

--- romanos/romanos.js
+++ romanos/romanos.js
@@ -1,53 +1,81 @@
-// Enteros y sus letras en Romano.
-var Arreglo = {
-	'M': 1000,
-	'CM': 900,
-	'D': 500,
-	'CD': 400,
-	'C': 100,
-	'XC': 90,
-	'L': 50,
-	'XL': 40,
-	'X': 10,
-	'IX': 9,
-	'V': 5,
-	'IV': 4,
-	'I': 1,
+// Mapa de números romanos.
+var Mapa = {
+	'M': 	1000,
+	'CM': 	900,
+	'D': 	500,
+	'CD': 	400,
+	'C': 	100,
+	'XC': 	90,
+	'L': 	50,
+	'XL': 	40,
+	'X': 	10,
+	'IX': 	9,
+	'V': 	5,
+	'IV': 	4,
+	'I': 	1,
 };
+
+// Agregamos la función "repeat" a las cadenas.
+// Permitira repetir una cadena X veces.
+String.prototype.repeat = function(i)
+{
+	return new Array(i + 1).join(this);
+}
 
 /**
  * Transforma el numero entero a numero romano.
  */
-function ARomano()
+function ARomano(iNum)
 {
 	// Obtenemos el entero que queremos transformar.
-	var pNum = parseInt(document.getElementById('valor').value);
+	// Del <input> de index.html
+	if ( iNum == undefined )
+		iNum = parseInt(document.getElementById('valor').value);
 
 	// ¡Es necesario un numero!
-	if ( pNum <= 0 )
+	if ( iNum <= 0 )
 		return alert('Escribe un numero valido mayor a 0.');
 
-	// Resultado final (Aqui iran las letras romanas)
-	var pResult = '';
+	var pResult 	= ''; 		// Resultado final (Aqui iran las letras romanas)
+	var pBuffer 	= []; 		// Aqui pondremos las letras en un array que podremos manejar.
 
-	for ( var i in Arreglo )
+	for ( var i in Mapa )
 	{
-		var pCantidad = Arreglo[i];
-
-		// Division entre la cantidad a convertir y el numero que podemos convertir a romano.
-		var pTmp = pNum / pCantidad;
-
-		// La division ha dado mayor o igual a 1, perfecto.
-		if ( pTmp >= 1 )
+		// Mientras el número a convertir esté en nuestro mapa.
+		while ( iNum >= Mapa[i] )
 		{
-			// Ponemos la cantidad de letras adecuada a la division.
-			for ( f = 1; f <= pTmp; ++f )
-			{
-				pNum 	-= pCantidad;
-				pResult += i;
-			}
+			// Al parecer iniciaremos el buffer de esta letra por primera vez.
+			// Ejemplo: pBuffer['M'] = 3;
+			if ( pBuffer[i] == undefined )
+				pBuffer[i] = 0;
+
+			// Agregamos la letra al buffer y restamos la cantidad.
+			iNum 		-= Mapa[i];
+			pBuffer[i] 	+= 1;
 		}
 	}
 
+	// Ahora verificamos el Buffer
+	for ( var l in pBuffer )
+	{
+		// Cantidad de veces que se repite esta letra.
+		var iCantidad = pBuffer[l];
+
+		// La letra es M (Mil) y se repite mayor a 4 veces.
+		// +4,000 Especial: Convertimos solo la cantidad de veces (4,5,6,etc) e ignoramos los miles.
+		if ( l == 'M' && iCantidad >= 4 )
+		{
+			pResult += ARomano(iCantidad);
+		}
+
+		// De otra forma solo repetimos las letras.
+		else
+		{
+			pResult += l.repeat(iCantidad);
+		}
+	}
+
+	// Imprimimos el resultado final.
 	document.getElementById('en_romano').innerHTML = pResult;
+	return pResult;
 }
